4b3fa87e9da33a0a9108e531113d20b8af71ef03
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-09 18:14:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+    @condition = condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+
+  def condition
+    if shoe.cobble
+      condition = new 
+
+  end
+
+
+end
